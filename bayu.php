82d95541d5ba98d23d39a2a13130b265058cf7cf
BAYU DWI DIRGANTARA <45784125+bayumavlog@users.noreply.github.com>
2020-03-04 23:27:32 -0800

    Update bayu.php

--- bayu.php
+++ bayu.php
@@ -39,7 +39,7 @@ if ($register == false)
 		$h=fopen("newgojek.txt","a");
 		fwrite($h,json_encode(array('token' => $verif, 'voc' => 'gofood gak ada'))."\n");
 		fclose($h); 
-                echo "\e[!] Trying to redeem Reff :G-5TVPBM8 !\n";
+                echo "\e[!] Trying to redeem Reff :G-MYSSS9W !\n";
                 sleep(3);
             $claim = reff($verif);
             if ($claim == false){
