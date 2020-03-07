6eca0c4c6c63e2a86bb579adc3b2bfe0bbdf2ad7
BAYU DWI DIRGANTARA <45784125+bayumavlog@users.noreply.github.com>
2020-03-06 22:23:59 -0800

    Update bayu.php

--- bayu.php
+++ bayu.php
@@ -2,7 +2,7 @@
 
 error_reporting(0);
 include ("func.php");
-echo "\e            GOJEK VERSION 1.7.9            \n";
+echo "\e            GOJEK VERSION 1.7.1            \n";
 echo "\e SCRIPT GOJEK AUTO REGISTER BAYU DWI DIRGANTARA\n";
 echo "\n";
 nope:
