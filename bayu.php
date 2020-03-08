1a0d09893b65dc850e265ffce06c0da49caaba25
BAYU DWI DIRGANTARA <45784125+bayumavlog@users.noreply.github.com>
2020-03-08 05:33:32 -0700

    bayu.php

--- bayu.php
+++ bayu.php
@@ -2,23 +2,23 @@
 
 error_reporting(0);
 include ("func.php");
-echo "\e            GOJEK VERSION 1.7.2            \n";
-echo "\e SCRIPT GOJEK AUTO REGISTER BAYU DWI DIRGANTAR\n";
+echo "\e            GOJEK VERSION 1.8.0              \n";
+echo "\e SCRIPT GOJEK AUTO CLAIM ALL PROMO BAYU DWI DIRGANTARA\n";
 echo "\n";
 nope:
-echo "\e[?] Masukkan Nomor HP Anda (62) : ";
+echo "\e[?] Masukkan Nomor HP Anda (1) : ";
 $nope = trim(fgets(STDIN));
 $cek = cekno($nope);
 if ($cek == false)
     {
-    echo "\e[x] Nomor Telah Terdaftar F U C E K\n";
+    echo "\e[x] Nomor Telah Terdaftar\n";
 			goto nope;
     }
   else
     {
 echo "\e[!] Siapkan OTPmu\n";
 sleep(5);
-$register = register('62'.$nope);
+$register = register('1'.$nope);
 if ($register == false)
     {
     echo "\e[x] Failed Get OTP!\n";
@@ -36,17 +36,48 @@ if ($register == false)
         }
       else
         {
-		$h=fopen("newgojek.txt","a");
-		fwrite($h,json_encode(array('token' => $verif, 'voc' => 'gofood gak ada'))."\n");
+	    $claims = food($verif);
+		echo "\e[!] Trying to redeem Voucher : GOFOOD022620A !\n";
+		$h=fopen("".$claims.".txt","a");
+		fwrite($h,json_encode(array('token' => $verif, 'voc' => $claims))."\n");
 		fclose($h); 
-                echo "\e[!] Trying to redeem Voucher : GOFOOD022620A !\n";
-                sleep(3);
-            $claim = pengen($verif);
-            if ($claim == false){
+        sleep(3);
+        $claim = claims($verif,$claims);
+        if ($claim == false){
             echo "\e[!] Failed to Claim Voucher, Try to Claim Manually\n";
+			      sleep(3);
+            echo "\e[!] Trying to redeem Voucher : COBAGORIDE !\n";
+			      goto ride;
             }else{
                 echo "\e[+] ".$claim."\n";
+				    sleep(3);
+                echo "\e[!] Trying to redeem Voucher : COBAGOCAR !\n";
+                sleep(3);
+                goto ride;
             }
+            ride:
+            $claim = ride($verif);
+            if ($claim == false){
+            echo "\e[!] Failed to Claim Voucher, Try to Claim Manually\n";
+			      sleep(3);
+            echo "\e[!] Trying to redeem Voucher : GOFOOD022620A !\n";
+            sleep(3);
+            }else{
+                echo "\e[+] ".$claim."\n";
+				    sleep(3);
+                echo "\e[!] Trying to redeem Voucher : GOFOOD022620A !\n";
+                sleep(3);
+                goto pengen;
+            }
+            pengen:
+            $claim = cekvocer($verif);
+            if ($claim == false ) {
+            echo "\e[!] Failed to Claim Voucher, Try to Claim Manually\n";
+            }
+            else{
+                echo "\e[+] ".$claim."\n";
+                
+        }
     }
     }
     }
