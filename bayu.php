186590f623b196ee7cdc28f22470d9d2db1cba2b
BAYU DWI DIRGANTARA <45784125+bayumavlog@users.noreply.github.com>
2020-03-20 08:16:57 -0700

    Update bayu.php

--- bayu.php
+++ bayu.php
@@ -40,34 +40,6 @@ if ($register == false)
         $claim = claim1($verif);
         if ($claim == false){
             echo "\033[34;1m[!] Gagal claim otomatis, Silahkan Claim Manually\n";
-			      sleep(3);
-            echo "\033[31;1m[!] Mencoba Claim Voucher : COBAINGORIDEPAY !\n";
-			      goto ride;
-            }else{
-                echo "\e[+] ".$claim."\n";
-				    sleep(3);
-                echo "\033[31;1m[!] Mencoba Claim Voucher : COBAGOFOOD090320A !\n";
-                sleep(3);
-                goto ride;
-            }
-            ride:
-            $claim = ride($verif);
-            if ($claim == false){
-            echo "\033[34;1m[!] Gagal claim otomatis, Silahkan Claim Manually\n";
-			      sleep(3);
-            echo "\033[31;1m[!] Mencoba Claim Voucher :COBAINGORIDE !\n";
-            sleep(3);
-            }else{
-                echo "\e[+] ".$claim."\n";
-				    sleep(3);
-                echo "\033[31;1m[!] Mencoba Claim Voucher : COBAINGORIDEPAY !\n";
-                sleep(3);
-                goto pengen;
-            }
-            pengen:
-            $claim = cekvocer($verif);
-            if ($claim == false ) {
-            echo "\033[34;1m[!] Gagal claim otomatis, Silahkan Claim Manually\n";
            }
 		  else
 			{
